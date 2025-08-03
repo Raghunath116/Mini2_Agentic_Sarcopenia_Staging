56cc400747bbe36f5095fcf1969b7b231ac4ce44
Raghunath Kadimi <raghunathkadimi@gmail.com>
2025-08-03 23:06:21 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# === Global Config for Project Paths ===
+
+# Raw dataset location (change if needed)
+DATA_DIR = "D:/DS_Mini_2"       # where your NSCLC-Radiomics data lives
+PROCESSED_DIR = "data/processed"  # where processed L3 slices will be saved
+LOG_DIR = "logs"                  # where all logs go
+
+# Project metadata
+PROJECT_NAME = "Mini2_Agentic_Sarcopenia_Staging"
+DATASET_NAME = "NSCLC-Radiomics"
