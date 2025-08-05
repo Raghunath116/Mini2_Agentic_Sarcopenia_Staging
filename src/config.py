3404dda9e1a9f0153baa805599a8d35fb7530f62
Raghunath Kadimi <raghunathkadimi@gmail.com>
2025-08-05 23:44:38 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,3 +8,14 @@ LOG_DIR = "logs"                  # where all logs go
 # Project metadata
 PROJECT_NAME = "Mini2_Agentic_Sarcopenia_Staging"
 DATASET_NAME = "NSCLC-Radiomics"
+
+# Paths for L3 extractor
+DATASET_DIR = r"D:\DS_Mini_2\manifest-1603198545583\NSCLC-Radiomics"
+OUTPUT_L3_DIR = r"outputs\l3_slices"
+LOGS_DIR = r"outputs\logs"
+FIGURES_DIR = r"outputs\figures"
+
+# L3 slice detection thresholds
+L3_Z_MIN = -250
+L3_Z_MAX = -100
+EXTRACTION_BUFFER = 5  # ± slices around L3 center
